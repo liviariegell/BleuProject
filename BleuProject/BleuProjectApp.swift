f7b217b0896113915269a795b7d0d30e9531e0ae
Livia Riegel <livia.riegel08@gmail.com>
2022-11-03 14:44:22 -0300

    Initial Commit

---
+++ BleuProject/BleuProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BleuProjectApp.swift
+//  BleuProject
+//
+//  Created by Ana Livia de Oliveira Riegel Machado on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BleuProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BleuProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BleuProject
+//
+//  Created by Ana Livia de Oliveira Riegel Machado on 03/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
