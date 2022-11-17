ff5735cbdb58f59ecdbeab3b1837c40c11968bf6
Livia Riegel <livia.riegel08@gmail.com>
2022-11-17 15:00:10 -0300

    Teste 2

--- BleuProject/ContentView.swift
+++ BleuProject/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
+            Text("Teste")
         }
     }
 }
